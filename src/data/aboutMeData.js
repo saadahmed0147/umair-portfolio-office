d6bfb11e9a6b720d5f6bbe0656e80969cbbffbc9
Saad Ahmed <saadahmed0147@gmail.com>
2024-11-06 18:12:37 +0500

    added project files

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,11 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: `Hello! I’m the founder of Code Dazzle, a forward-thinking software house where we specialize in creating innovative digital solutions. With nearly 12 years of hands-on experience as a software engineer, I’ve dedicated my career to building efficient, user-centered applications across various industries At Code Dazzle, our mission is to help clients transform their ideas into digital realities. We’re committed to innovation, quality, and building solutions that drive meaningful impact.
+`	},
+	{
+		id: 2,
+		bio: `My goal is to elevate Code Dazzle to the next level, enhancing our offerings and expertise. I aim to foster a culture of continuous improvement and innovation within the team. By embracing new technologies, we can better serve our clients' evolving needs. Ultimately, I aspire to create impactful solutions that make a difference in the digital landscape.
+
+`	},
+];
